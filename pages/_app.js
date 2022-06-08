af168d3a919db2d61ef719c8ebceda4db655ef95
Lambert Theisen <lambert.theisen@rwth-aachen.de>
2022-06-08 12:43:59 +0200

    Cleanup

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,7 @@ import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
-    import("bootstrap/dist/js/bootstrap");
+    import("bootstrap/dist/js/bootstrap.bundle");
   }, []);
   return (
     <Component {...pageProps} />
--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,7 @@ export default function Home() {
           <ProjectCard></ProjectCard>
 
           <div className="col">
-            <div className="card card-cover h-100 overflow-hidden text-white bg-dark rounded-4 shadow-lg" style={{ backgroundImage: "url('unsplash-photo-2.jpg')" }}>
+            <div className="card card-cover h-100 overflow-hidden bg-primary bg-gradient rounded-4 shadow-lg">
               <div className="d-flex flex-column h-100 p-5 pb-3 text-white text-shadow-1">
                 <h2 className="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold">
                   Much longer title that wraps to multiple lines
