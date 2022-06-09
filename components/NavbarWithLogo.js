cf793fcf8d29e798ba7237842a1299983e041977
Lambert Theisen <lambert.theisen@rwth-aachen.de>
2022-06-09 12:06:07 +0200

    Fix height

--- components/NavbarWithLogo.js
+++ components/NavbarWithLogo.js
@@ -20,7 +20,7 @@ export default function NavbarWithLogo() {
     <nav className="navbar navbar-expand-lg bg-light">
       <div className="container">
         <a className="navbar-brand d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none" href="#">
-          <img src="/logo_text_rwth.png" alt="" height="60" className="d-inline-block align-text-top"></img>
+          <img src="/logo_text_rwth.png" alt="" height="56" className="d-inline-block align-text-top"></img>
         </a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
